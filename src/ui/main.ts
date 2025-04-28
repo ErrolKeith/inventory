6d0ef77f33a165ed414c29875e69242624bd8804
ErrolKeith <errol.k.keith@gmail.com>
2025-04-28 12:39:11 -0400

    fix mount element id

--- src/ui/main.ts
+++ src/ui/main.ts
@@ -4,7 +4,7 @@ import App from "./App.vue";
 import "./style.css";
 
 window.addEventListener("DOMContentLoaded", () => {
-  const mountEl = document.getElementById("apps");
+  const mountEl = document.getElementById("app");
   if (!mountEl) {
     throw Error("Mount element missing");
   }
